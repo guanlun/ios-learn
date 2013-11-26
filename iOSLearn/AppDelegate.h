dc195a03a01e40b81c024a25ef29f212cd0b73ef
Guanlun Zhao <guanlunzhao@gmail.com>
2013-11-26 21:14:48 +0800

    Initial Commit

---
+++ iOSLearn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSLearn
+//
+//  Created by Guanlun Zhao on 11/26/13.
+//  Copyright (c) 2013 Guanlun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSLearn/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSLearn
+//
+//  Created by Guanlun Zhao on 11/26/13.
+//  Copyright (c) 2013 Guanlun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
